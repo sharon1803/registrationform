d4d0f975aac1ee955c626f671108e2e8944079a7
sharon1803 <sharonsaini1803@gmail.com>
2024-12-26 21:00:48 +0530

    Create process.php

---
+++ process.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $phone = htmlspecialchars($_POST['phone']);
+
+  echo json_encode([
+    'success' => true,
+    'name' => $name,
+    'email' => $email,
+    'phone' => $phone
+  ]);
+  exit();
+}
+
+echo json_encode(['success' => false, 'message' => 'Invalid request']);
+?>
